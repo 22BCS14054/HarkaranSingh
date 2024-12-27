70df10504ede77c6f075dc97debc74ad20e15b3a
22BCS14054 <balakaran32@gmail.com>
2024-12-27 16:51:33 +0530

    Create journalroutes.js

---
+++ journalroutes.js
@@ -0,0 +1,22 @@
+// File 3: routes/journalRoutes.js (Journal API Routes)
+const express = require('express');
+const router = express.Router();
+const Journal = require('../models/Journal');
+const { analyzeEmotion } = require('../utils/aiUtils');
+
+// Add a new journal entry
+router.post('/', async (req, res) => {
+  try {
+    const { userId, content } = req.body;
+    const emotionAnalysis = await analyzeEmotion(content);
+    const newJournal = new Journal({
+      user: userId,
+      content,
+      emotion: emotionAnalysis,
+    });
+    const savedJournal = await newJournal.save();
+    res.status(201).json(savedJournal);
+  } catch (err) {
+    res.status(500).json({ message: 'Error saving journal entry', error: err });
+  }
+});
